4cb2e0b3fd2c98ca83e8d9aed878640596ca1bc3
my pj <167681477+iamatimnl@users.noreply.github.com>
2025-06-05 22:22:05 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,4 +4,16 @@ app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
 @app.route('/')
 def home():
-    return render_template('index.html')  # Template file should be added later
+    return render_template('index.html')
+
+@app.route('/pos')
+def pos():
+    return render_template('pos.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
