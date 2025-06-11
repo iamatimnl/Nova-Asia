f8440b71eb4949a95b52b854da516b41aad11bf2
my pj <167681477+iamatimnl@users.noreply.github.com>
2025-06-11 15:31:14 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,5 @@
-# wsgi.py
-from notifier import app, socketio  # ✅ 现在从 notifier.py 中导入
-
-def run():
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+## wsgi.py
+from app import app, socketio
 
 if __name__ == "__main__":
-    run()
-
+    socketio.run(app)
