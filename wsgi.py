99628f26d4015a0bbb4d405372ded91c565cc007
my pj <167681477+iamatimnl@users.noreply.github.com>
2025-06-21 23:20:03 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,3 @@
-from app import app, socketio
+from app import socketio, app
+socketio.run(app, host="0.0.0.0", port=5000)
 
-# 让 gunicorn 能找到 Flask 实例
-application = app
-
-# 如果你本地运行 wsgi.py，也可以启动 socketio（备用）
-if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
